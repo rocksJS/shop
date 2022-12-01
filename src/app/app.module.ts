50c3f8aabc1119e522350c25706494ba2e7acdac
rocksJS <6abyghost@gmail.com>
2022-12-01 13:07:21 +0300

    added logo, commit before making translation shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatSlideToggleModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,9 +3,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NewsSubscribitionComponent } from './components/news-subscribition/news-subscribition.component';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
-  declarations: [MainPageComponent, HeaderComponent, NewsSubscribitionComponent],
-  imports: [MainRoutingModule],
+  declarations: [
+    MainPageComponent,
+    HeaderComponent,
+    NewsSubscribitionComponent,
+  ],
+  imports: [MainRoutingModule, MatIconModule],
 })
 export class MainModule {}
