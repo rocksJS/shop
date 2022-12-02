b221bab2dca2dd1af4f8ea1f4e4b509ee1d99132
rocksJS <6abyghost@gmail.com>
2022-12-02 18:25:35 +0300

    added routerlink, pushed. before add dataview.

--- src/app/modules/main/components/header/header.component.ts
+++ src/app/modules/main/components/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageValues } from 'src/app/shared/interfaces/language-values.interface';
-
+import {
+  INFO_PATH,
+  GALLERY_PATH,
+} from 'src/app/shared/constants/routing-path.const';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public readonly infoPath = INFO_PATH;
+
+  public readonly galleryPath = GALLERY_PATH;
+
   constructor() {}
 
   ngOnInit(): void {}
